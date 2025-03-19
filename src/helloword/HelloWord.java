85fab46f919251f46ab075d451711d1de80e908c
Lida <maulida211231@gmail.com>
2025-03-19 16:56:56 +0700

    apa kabar

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -16,6 +16,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word");
         System.out.println ("My name is Lida");
+        System.out.println("apa kabar");
     }
     
 }
